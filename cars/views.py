3a28067025541402fa87aff2bf257f64d87e39ca
txhung <txhung@fresher-ub-01.rd.local>
2023-07-17 08:29:42 +0700

    add update and delete method

--- cars/views.py
+++ cars/views.py
@@ -14,7 +14,7 @@ def get_cars(request):
             'price': car.price,
         }
         car_list.append(car_data)
-    return JsonResponse(car_list, safe=False)
+    return JsonResponse(car_list, safe=False, status=200)
 
 @csrf_exempt
 def create_car(request):
@@ -29,3 +29,30 @@ def create_car(request):
         car.save()
         
         return JsonResponse({'message': 'Car created successfully'}, status=201)
+    
+@csrf_exempt
+def update_car(request, car_id):
+    if request.method == 'PUT':
+        try:
+            car = Car.objects.get(id=car_id)
+            data = json.loads(request.body)
+            car.make = data.get('make', car.make)
+            car.model = data.get('model', car.model)
+            car.year = data.get('year', car.year)
+            car.price = data.get('price', car.price)
+            car.save()
+            return JsonResponse({'message': 'Car updated successfully'})
+        except Car.DoesNotExist:
+            return JsonResponse({'message': 'Car not found'}, status=404)
+        
+@csrf_exempt
+def delete_car(request, car_id):
+    if request.method == 'DELETE':
+        try:
+            car = Car.objects.get(id=car_id)
+            car.delete()
+            return JsonResponse({'message': 'Car deleted successfully'})
+        except Car.DoesNotExist:
+            return JsonResponse({'message': 'Car not found'}, status=404)
+
+
